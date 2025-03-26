00f563b3f2f39c61fbda596199bcabeb5115b989
Alexandra0707 <alex07marta@gmail.com>
2025-03-26 18:25:18 +0000

    sssdf

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import ctrlWrapper from '../utils/ctrlWrapper.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   getAllContactsController,
   getContactByIdController,
@@ -10,9 +10,11 @@ import {
 import { validateBody } from '../middlewares/validateBody.js';
 import { createContactValidationSchema } from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
+router.use(authenticate);
 router.get('/', ctrlWrapper(getAllContactsController));
 router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 router.post(
--- src/server.js
+++ src/server.js
@@ -1,30 +1,31 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { getEnvVar } from './utils/getEnvVar.js';
-import contactsRouter from './routers/contacts.js';
-import { errorHandler } from './middlewares/errorHandler.js';
-import notFoundHandler from './middlewares/notFoundHandler.js';
 
-const PORT = getEnvVar('PORT');
+import router from './routers/index.js';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
+
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
+  app.use(pino());
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World!',
+    });
+  });
 
-  app.use('/contacts', contactsRouter);
-
-  app.use(notFoundHandler);
-
+  app.use(router);
+  app.use('*', notFoundHandler);
   app.use(errorHandler);
 
   app.listen(PORT, () => {
