45b3e73b5ab373570ead830ee93ea01f642c849d
Alexandra0707 <alex07marta@gmail.com>
2025-03-31 17:09:49 +0100

    dd

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,10 +8,8 @@ import {
   deleteContactController,
 } from '../controllers/contacts.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import {
-  createContactValidationSchema,
-  updateContactValidationSchema,
-} from '../validation/contacts.js';
+import { createContactValidationSchema } from '../validation/contacts.js';
+import updateContactValidationSchema from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
