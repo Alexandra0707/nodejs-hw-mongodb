5b518fca3c729814049a1a6b09950ba580f3b4aa
Alexandra0707 <alex07marta@gmail.com>
2025-04-05 18:45:41 +0100

    ll

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -3,7 +3,6 @@ import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 import { SORT_ORDER } from '../constants/index.js';
 
 export const getAllContacts = async ({
-  userId,
   page = 1,
   perPage = 10,
   sortOrder = SORT_ORDER.ASC,
@@ -12,7 +11,8 @@ export const getAllContacts = async ({
 }) => {
   const limitOnPage = perPage;
   const offset = (page - 1) * perPage;
-  const contactsQuery = contactsCollection.find({ userId });
+
+  const contactsQuery = contactsCollection.find({ userId: filter.userId });
 
   if (filter.type) {
     contactsQuery.where('contactType').equals(filter.type);
@@ -22,10 +22,7 @@ export const getAllContacts = async ({
     contactsQuery.where('isFavourite').equals(filter.isFavourite);
   }
 
-  const contactsCount = await contactsCollection
-    .find({ userId })
-    .merge(contactsQuery)
-    .countDocuments();
+  const contactsCount = await contactsQuery.clone().countDocuments();
 
   const contacts = await contactsQuery
     .skip(offset)
@@ -40,6 +37,44 @@ export const getAllContacts = async ({
     ...paginationData,
   };
 };
+
+// export const getAllContacts = async ({
+//   page = 1,
+//   perPage = 10,
+//   sortOrder = SORT_ORDER.ASC,
+//   sortBy = '_id',
+//   filter = {},
+// }) => {
+//   const limitOnPage = perPage;
+//   const offset = (page - 1) * perPage;
+//   const contactsQuery = contactsCollection.find({ userId });
+
+//   if (filter.type) {
+//     contactsQuery.where('contactType').equals(filter.type);
+//   }
+
+//   if (typeof filter.isFavourite === 'boolean') {
+//     contactsQuery.where('isFavourite').equals(filter.isFavourite);
+//   }
+
+//   const contactsCount = await contactsCollection
+//     .find({ userId })
+//     .merge(contactsQuery)
+//     .countDocuments();
+
+//   const contacts = await contactsQuery
+//     .skip(offset)
+//     .sort({ [sortBy]: sortOrder })
+//     .limit(limitOnPage)
+//     .exec();
+
+//   const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+//   return {
+//     data: contacts,
+//     ...paginationData,
+//   };
+// };
 
 export const getContactById = async (contactId, userId) => {
   return await contactsCollection.findOne({ _id: contactId, userId });
